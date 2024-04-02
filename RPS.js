18a76f4ae26cb0de335ef8564f4670695a7efd75
S0ulShell <rajatpkumar24@gmail.com>
2024-04-02 20:13:01 +1300

    completed functionality

--- RPS.js
+++ RPS.js
@@ -1,61 +1,101 @@
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+
+
+
+
+
 function getComputerChoice() {
-    const ROCK = "rock";
-    const PAPER = "paper";
-    const SCISSORS = "scissors"
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors"
     let selector = Math.floor(Math.random() * 3)
     switch (selector) {
         case 0:
             // console.log(ROCK)
-            return ROCK;
+            return rock;
             break;
         case 1:
             // console.log(PAPER)
-            return PAPER;
+            return paper;
             break;
         case 2:
             // console.log(SCISSORS)
-            return SCISSORS;
+            return scissors;
             break;
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+
     if (playerSelection === computerSelection) {
         return ("Its a tie! " + playerSelection + " and " + computerSelection + " are equal...");
-    }else if ((playerSelection === "rock" && computerSelection === "scissors" || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper"))) {
+    } else if ((playerSelection === "rock" && computerSelection === "scissors" || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper"))) {
         winCounter += 1;
+        document.getElementById('winCounter').innerHTML = winCounter;
         return ("You win! " + playerSelection + " beats " + computerSelection + "!");
-    }else {
+    } else {
+        lossCounter += 1;
+        document.getElementById("lossCounter").innerHTML = lossCounter;
         return ("You lose! " + playerSelection + " loses to " + computerSelection + "!");
     }
 }
 
-function getPlayerSelection() {
-
-    playerSelection = prompt("Rock Paper or Scissors?")
-    return(playerSelection);
+function getPlayerSelection(string) {
+    playerSelection = string;
+    console.log(playerSelection);
+    return playerSelection;
 }
 
-function playGame() {
+function playGame(playerSelection) {
+    computerSelection = getComputerChoice();
 
-    let while_counter = 0;
-    while(while_counter < 5){
-        computerSelection = getComputerChoice();
-        playerSelection = getPlayerSelection();
-        console.log(playRound(playerSelection, computerSelection));
-        while_counter += 1;
+    console.log(playRound(playerSelection, computerSelection));
+    if (winCounter < 5 || lossCounter < 5){ 
+        document.getElementById("message").innerHTML = "";
     }
-    if(winCounter > 2){
-        console.log("You've won! Your Score: " + winCounter);
+
+    if (winCounter == 5 || lossCounter == 5) {
+        if (winCounter > lossCounter) {
+            document.getElementById("message").innerHTML = "You win! You scored " + winCounter + " and lost " + lossCounter + "!";
+            winCounter = 0;
+            lossCounter = 0;
+            document.getElementById("lossCounter").innerHTML = lossCounter;
+            document.getElementById('winCounter').innerHTML = winCounter;
+        } else if (winCounter < lossCounter) {
+            document.getElementById("message").innerHTML = "You lose! You scored " + winCounter + " and lost " + lossCounter + "!";
+            winCounter = 0;
+            lossCounter = 0;
+            document.getElementById("lossCounter").innerHTML = lossCounter;
+            document.getElementById('winCounter').innerHTML = winCounter;
+        } else if (winCounter == lossCounter) {
+            document.getElementById("message").innerHTML = "It's a tie! You scored " + winCounter + " and lost " + lossCounter + "!";
+            winCounter = 0;
+            lossCounter = 0;
+            document.getElementById("lossCounter").innerHTML = lossCounter;
+            document.getElementById('winCounter').innerHTML = winCounter;
+        }
     }
-    else{
-        console.log("You've Lost :( Your Score: " + winCounter)
-    }
-    
 }
 
 let winCounter = 0;
+document.getElementById('winCounter').innerHTML = winCounter; 
+let lossCounter = 0;
+document.getElementById("lossCounter").innerHTML = lossCounter;
 let playerSelection;
 let computerSelection = getComputerChoice();
-playGame();
+
+
+
+const rockBtn = document.querySelector("#rockButton");
+rockBtn.addEventListener("click", () => { getPlayerSelection(rock) });
+rockBtn.addEventListener("click", () => { playGame(playerSelection) })
+
+const paperBtn = document.querySelector("#paperButton");
+paperBtn.addEventListener("click", () => { getPlayerSelection(paper) });
+paperBtn.addEventListener("click", () => { playGame(playerSelection) })
+
+const scissorBtn = document.querySelector("#scissorButton");
+scissorBtn.addEventListener("click", () => { getPlayerSelection(scissors) });
+scissorBtn.addEventListener("click", () => { playGame(playerSelection) })
